a2eab38de05ca762eec803e8b66ae04f29a626b9
Roman Ceresnak <romanceresnak@windowslive.com>
2025-03-05 09:57:59 +0100

    Update name

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,12 @@ resource "azurerm_resource_group" "example" {
   
   tags = {
     environment = "development"
-    project     = "azure-terraform-demo"
+    project     = "azure-terraform-demo-rc"
   }
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "mojstorage12345" 
+  name                     = "mojstorage12345-rcas" 
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
